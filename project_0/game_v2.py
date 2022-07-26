440d568168bfca0c64a7d905e7292a54075fcb6b
Yan317 <igor_kuz@list.ru>
2022-07-26 16:19:18 +0300

    execution project 0

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -23,11 +23,11 @@ def random_predict(number:int=1) -> int:
     return(count)
 
 
-def score_game(random_predict) -> int:
+def score_game(def_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадает наш алгоритм
 
     Args:
-        random_predict (_type_): функция угадывания
+        def_predict (_type_): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -38,7 +38,7 @@ def score_game(random_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список числе
     
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(def_predict(number))
     
     score = int(np.mean(count_ls)) # находим среднее количество попыток
     
@@ -46,6 +46,34 @@ def score_game(random_predict) -> int:
     return(score)
 
 
+def optimal_predict(number:int=1) -> int:
+    '''Угадываем число за минимальное количество поыток
+    
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1
+        
+    Returns:
+        int: Число попыток
+    '''
+    
+    count = 0   # обнуляем счётчик попыток
+    min_number = 1  # минамально число для угадывания
+    max_number = 101  # максимальное число для угадывания (!!! нужно больше 100 для возможности выбрать 100 при делении между min и max)
+    
+    while True:
+        count += 1
+        if min_number != max_number:    # если диапозон для угадывания более одного числа
+            predict_number = min_number + (max_number-min_number)//2    # всегда для проверки выбираем число посередине
+            #print(predict_number)
+            if predict_number < number:
+                min_number = predict_number
+            elif predict_number > number:
+                max_number = predict_number
+            else:
+                break   # если не больше и не меньше, значит угадали => вызожим из цикла
+    return(count)
+    
+
 # RUN
 if __name__ == '__name__':
-    score_game(random_predict)
+    score_game(optimal_predict)
